7e243c8925b93f3b94b710604635c1b2de05517f
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2024-05-09 23:27:02 +0100

    implement What-We-Do section (desktop only)

--- src/components/layouts/LandingPage/WhatWeDo/WhatWeDo.tsx
+++ src/components/layouts/LandingPage/WhatWeDo/WhatWeDo.tsx
@@ -1,33 +1,39 @@
 import shipmentVideo from "@assets/images/shipment.mp4";
 import Section from "@components/UI/Section/Section";
+import styles from "./whatWeDo.module.css";
 
 export default function WhatWeDo() {
   return (
     <Section>
-      <div>
-        <h1>Lorem ipsum dolor sit amet consectetur.</h1>
-        <div>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Exercitationem repudiandae doloremque aliquam, obcaecati
-            necessitatibus perferendis.
-          </p>
-          <div>
+      <div className={styles.doings}>
+        <div className={styles.doingsTextContent}>
+          <div className={styles.doingsText}>
+            <h1>
+              <span>seamless logistics</span> <br /> for a connected world
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Exercitationem repudiandae doloremque aliquam, obcaecati
+              necessitatibus perferendis.
+            </p>
+          </div>
+        </div>
+        <div className={styles.videoHolder}>
+          <video width="750" autoPlay loop muted>
+            <source src={shipmentVideo} type="video/mp4" />
+            Your browser does not support the video tag.
+            {/* // video credit: Video by Jay S: https://www.pexels.com/video/a-railway-crossing-4021642/ */}
+          </video>
+
+          <div className={styles.statsHolder}>
             {Array.from({ length: 4 }, (_, index) => (
-              <span key={index}>
+              <span key={index} className={styles.stats}>
                 <h1>20k</h1>
                 <p>Lorem, ipsum.</p>
               </span>
             ))}
           </div>
         </div>
-      </div>
-      <div>
-        <video width="750" autoPlay loop muted>
-          <source src={shipmentVideo} type="video/mp4" />
-          Your browser does not support the video tag.
-          {/* // video credit: Video by Jay S: https://www.pexels.com/video/a-railway-crossing-4021642/ */}
-        </video>
       </div>
     </Section>
   );
