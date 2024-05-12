892d87f829b232203a8dbf32794c68477534928e
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2024-05-12 04:51:58 +0100

    implement footer section (desktop only)

--- src/components/layouts/LandingPage/Footer/Footer.tsx
+++ src/components/layouts/LandingPage/Footer/Footer.tsx
@@ -1,12 +1,25 @@
 import Section from "@components/UI/Section/Section";
+import styles from "./footer.module.css";
 
 export default function Footer() {
   return (
-    <footer className="">
-      <Section>
-        {/* add absolute position here */}
-        <h1>Footer</h1>
-      </Section>
-    </footer>
+    <Section>
+      <footer className={styles.footer}>
+        <div>
+          <h1>Hermes</h1>
+        </div>
+        <div>
+          <ul>
+            <li>terms</li>
+            <li>policy</li>
+            <li>contact</li>
+          </ul>
+          <p>
+            &copy; {new Date().getFullYear()} Hermes Logistics. All rights
+            reserved.
+          </p>
+        </div>
+      </footer>
+    </Section>
   );
 }
