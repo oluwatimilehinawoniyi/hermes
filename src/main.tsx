d117acc12d13ecba58c17e9e9eed97c99da7c565
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2024-05-15 22:19:16 +0100

    correct app error

--- src/main.tsx
+++ src/main.tsx
@@ -3,23 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Home, Login } from "./pages";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
     <App />
   </React.StrictMode>
 );
