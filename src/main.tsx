6b224540f403a37f9cda8078c026ef6f96d0f60b
Adetule-ayomide <adetuleayomide@gmail>
2024-05-15 16:14:51 +0100

    declutter file and downgrade router version

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import App from "./App";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Home, Login } from "./pages";
@@ -19,5 +20,6 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
