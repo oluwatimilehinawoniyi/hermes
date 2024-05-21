6546aebac10dc98e3358b07f68d2ffc600bdb08d
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2024-05-21 12:57:40 +0100

    adjust dashboard layout

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -5,8 +5,22 @@ import Sidebar from "@components/layouts/Dashboard/Sidebar/Sidebar";
 export default function Dashboard() {
   return (
     <section className={style.dashboard}>
-      <Sidebar />
-      <Outlet />
+      <section
+        style={{
+          width: "18%",
+        }}
+      >
+        <Sidebar />
+      </section>
+      <section
+        style={{
+          width: "82%",
+          backgroundColor: "var(--line-colour)",
+          padding: "1rem",
+        }}
+      >
+        <Outlet />
+      </section>
     </section>
   );
 }
