bd129daf06790674e1a88b4f81807fdb12a49b73
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2024-05-28 14:50:06 +0100

    update client routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Auth, Dashboard, Home, Login, SignUp, AuthRoute } from "@pages/index";
 import { AppHome, Requests, Clients, Parcels, Shipment } from "@pages/app";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="auth" element={<Auth />}>
-          <Route index element={<Login />} />
-          <Route path="signup" element={<SignUp />} />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="auth" element={<Auth />}>
+        <Route index element={<Login />} />
+        <Route path="signup" element={<SignUp />} />
+      </Route>
+      <Route element={<AuthRoute />}>
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route index element={<AppHome />} />
+          <Route path="requests" element={<Requests />} />
+          <Route path="clients" element={<Clients />} />
+          <Route path="parcels" element={<Parcels />} />
+          <Route path="shipment" element={<Shipment />} />
         </Route>
-        <Route element={<AuthRoute />}>
-          <Route path="/dashboard" element={<Dashboard />}>
-            <Route index element={<AppHome />} />
-            <Route path="requests" element={<Requests />} />
-            <Route path="clients" element={<Clients />} />
-            <Route path="parcels" element={<Parcels />} />
-            <Route path="shipment" element={<Shipment />} />
-          </Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+      </Route>
+    </Routes>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import "./index.css";
 import "@assets/styles/scroll.css";
 import App from "./App";
 import AuthProvider from "@context/AuthProvider";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </AuthProvider>
   </React.StrictMode>
 );
